7394fc07d3c485cff6a662b6b525f1c4dc12a71b
Vivart Pandey <vivart.pandey@gmail.com>
2021-02-27 12:27:28 +0000

    fixed style issues

--- app/src/main/java/com/example/androiddevchallenge/ui/NavGraph.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/NavGraph.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui
 
 import androidx.compose.animation.ExperimentalAnimationApi
@@ -5,7 +20,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
-import androidx.navigation.compose.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
 import com.example.androiddevchallenge.data.Repository
 import com.example.androiddevchallenge.ui.detail.DetailScreen
 import com.example.androiddevchallenge.ui.home.HomeScreen
@@ -30,9 +49,11 @@ fun NavGraph(repository: Repository) {
         }
         composable(
             MainDestinations.DETAIL,
-            arguments = listOf(navArgument(MainDestinations.DETAIL_ARG) {
-                type = NavType.StringType
-            })
+            arguments = listOf(
+                navArgument(MainDestinations.DETAIL_ARG) {
+                    type = NavType.StringType
+                }
+            )
         ) { backStackEntry ->
             val id = backStackEntry.arguments?.getString(MainDestinations.DETAIL_ARG)
             id?.let { DetailScreen(id = id, repository = repository, onBack = actions.goBack) }
--- app/src/main/java/com/example/androiddevchallenge/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/home/HomeScreen.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.home
 
 import androidx.compose.animation.AnimatedVisibility
@@ -6,14 +21,35 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -88,11 +124,13 @@ fun HomeContent(list: List<PuppyData>, onNext: (String) -> Unit, modifier: Modif
             exit = fadeOut(),
             modifier = Modifier.align(Alignment.BottomCenter)
         ) {
-            ScrollToTopButton(onClick = {
-                coroutineScope.launch {
-                    listState.animateScrollToItem(index = 0)
+            ScrollToTopButton(
+                onClick = {
+                    coroutineScope.launch {
+                        listState.animateScrollToItem(index = 0)
+                    }
                 }
-            })
+            )
         }
     }
 }
