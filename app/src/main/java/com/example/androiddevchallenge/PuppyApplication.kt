ffc1e71ec5bf57e742a8e1032a8606bad144b6fb
Vivart Pandey <vivart.pandey@gmail.com>
2021-12-17 09:50:42 +0000

    upgrade dependency versions and fixed one koin issue

--- app/src/main/java/com/example/androiddevchallenge/PuppyApplication.kt
+++ app/src/main/java/com/example/androiddevchallenge/PuppyApplication.kt
@@ -17,16 +17,18 @@ package com.example.androiddevchallenge
 
 import android.app.Application
 import com.example.androiddevchallenge.di.appModule
+import org.koin.android.BuildConfig
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
+import org.koin.core.logger.Level
 
 class PuppyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         // Start Koin
         startKoin {
-            androidLogger()
+            androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
             androidContext(this@PuppyApplication)
             modules(appModule)
         }
